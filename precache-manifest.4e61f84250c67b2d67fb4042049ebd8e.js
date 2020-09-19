abca8d547191ce8402704046e90804014ad486d3
Nikita-12345 <singh.nkta@gmail.com>
2020-09-19 17:46:36 +0530

    Updates

---
+++ precache-manifest.4e61f84250c67b2d67fb4042049ebd8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6690d940d43b65538e399e9c6fb42d",
+    "url": "/sapient_task_19-09-2020/index.html"
+  },
+  {
+    "revision": "b914742139f64a259900",
+    "url": "/sapient_task_19-09-2020/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "ded961425e3084b19837",
+    "url": "/sapient_task_19-09-2020/static/css/main.c5a8745f.chunk.css"
+  },
+  {
+    "revision": "b914742139f64a259900",
+    "url": "/sapient_task_19-09-2020/static/js/2.01919376.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sapient_task_19-09-2020/static/js/2.01919376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded961425e3084b19837",
+    "url": "/sapient_task_19-09-2020/static/js/main.2257de10.chunk.js"
+  },
+  {
+    "revision": "f7f3822c2ca722280b62",
+    "url": "/sapient_task_19-09-2020/static/js/runtime-main.97fa2ce4.js"
+  }
+]);
